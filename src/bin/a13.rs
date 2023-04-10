d00775b99bcdb3de19700124ab2de38346f5541d
Hadi Haider <haiderhadi055@gmail.com>
2023-04-10 07:00:41 +0500

    Lesson-13: Vectors and for loops

--- src/bin/a13.rs
+++ src/bin/a13.rs
@@ -10,4 +10,19 @@
 // * Determine whether to print the number or print "thirty" inside the loop
 // * Use the .len() function to print the number of elements in a vector
 
-fn main() {}
+fn main() {
+    // * Use a vector to store 4 numbers
+    let my_numbers = vec![10, 20, 30, 40];
+
+    // * Use the .len() function to print the number of elements in a vector
+    println!("The length = {:?}", my_numbers.len());
+
+    // * Iterate through the vector using a for..in loop
+    for my_num in my_numbers {
+        match my_num {
+            // * Determine whether to print the number or print "thirty" inside the loop
+            30 => println!("The number is = thirty"),
+            _ => println!("The number is = {:?}", my_num),
+        }
+    }
+}
