adf0252b442a263937a8b18e002184f58a4a5817
Hadi Haider <haiderhadi055@gmail.com>
2023-04-10 06:37:35 +0500

    Lesson: Ownership

--- src/bin/a11.rs
+++ src/bin/a11.rs
@@ -9,4 +9,29 @@
 // * Create a function to display the quantity, with the struct as a parameter
 // * Create a function to display the id number, with the struct as a parameter
 
-fn main() {}
+// * Use a struct for the grocery item
+struct GroceryItem {
+    // * Use two i32 fields for the quantity and id number
+    id: i32,
+    quantity: i32,
+}
+
+// * Create a function to display the quantity, with the struct as a parameter
+fn display_quantity(grocery: &GroceryItem) {
+    println!("Quantity = {:?}", grocery.quantity)
+}
+
+// * Create a function to display the id number, with the struct as a parameter
+fn display_id(grocery: &GroceryItem) {
+    println!("ID = {:?}", grocery.id)
+}
+
+fn main() {
+    let grocery_item = GroceryItem {
+        id: 32,
+        quantity: 32,
+    };
+
+    display_id(&grocery_item);
+    display_quantity(&grocery_item);
+}
--- src/bin/a12.rs
+++ src/bin/a12.rs
@@ -10,4 +10,66 @@
 // * Implement functionality on the box struct to create a new box
 // * Implement functionality on the box struct to print the characteristics
 
-fn main() {}
+// * Use an enum for the box color
+enum BoxColor {
+    Brown,
+    Yellow,
+}
+
+impl BoxColor {
+    fn print(&self) {
+        match self {
+            BoxColor::Brown => println!("Color = Brown"),
+            BoxColor::Yellow => println!("Color = Yellow"),
+        }
+    }
+}
+
+struct Dimensions {
+    width: f64,
+    height: f64,
+    depth: f64,
+}
+
+impl Dimensions {
+    fn print(&self) {
+        println!("Width = {:?}", self.width);
+        println!("Height = {:?}", self.height);
+        println!("Depth = {:?}", self.depth);
+    }
+}
+// * Use a struct to encapsulate the box characteristics
+struct ShippingBox {
+    dimensions: Dimensions,
+    weight: f64,
+    color: BoxColor,
+}
+
+impl ShippingBox {
+    // * Implement functionality on the box struct to create a new box
+    fn create_box(weight: f64, dimensions: Dimensions, color: BoxColor) -> Self {
+        Self {
+            weight,
+            dimensions,
+            color,
+        }
+    }
+
+    // * Implement functionality on the box struct to print the characteristics
+    fn display_char(&self) {
+        self.dimensions.print();
+        self.color.print();
+        println!("Weight = {:?}", self.weight);
+    }
+}
+
+fn main() {
+    let box_dimensions = Dimensions {
+        width: 3.0,
+        height: 2.0,
+        depth: 1.0,
+    };
+
+    let shipping_box = ShippingBox::create_box(3.1, box_dimensions, BoxColor::Brown);
+    shipping_box.display_char();
+}
