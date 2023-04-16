5964cdfecfb0598f0596abb34170aa42dabc8867
Hadi Haider <haiderhadi055@gmail.com>
2023-04-16 08:03:21 +0500

    Lesson-14: Strings

--- src/bin/a13.rs
+++ src/bin/a13.rs
@@ -14,15 +14,15 @@ fn main() {
     // * Use a vector to store 4 numbers
     let my_numbers = vec![10, 20, 30, 40];
 
-    // * Use the .len() function to print the number of elements in a vector
-    println!("The length = {:?}", my_numbers.len());
-
     // * Iterate through the vector using a for..in loop
-    for my_num in my_numbers {
+    for my_num in &my_numbers {
         match my_num {
             // * Determine whether to print the number or print "thirty" inside the loop
             30 => println!("The number is = thirty"),
             _ => println!("The number is = {:?}", my_num),
         }
     }
+
+    // * Use the .len() function to print the number of elements in a vector
+    println!("The length = {:?}", my_numbers.len());
 }
--- src/bin/a14.rs
+++ src/bin/a14.rs
@@ -11,4 +11,44 @@
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
 
-fn main() {}
+// * Use a struct for a persons age, name, and favorite color
+struct Person {
+    age: i32,
+    // * The color and name should be stored as a String
+    name: String,
+    fav_color: String,
+}
+
+// * The name and colors should be printed using a function
+fn print_data(name: &str, color: &str) {
+    println!("name: {:?}, favorite color: {:?}", name, color)
+}
+
+fn main() {
+    // * Create and store at least 3 people in a vector
+    let persons = vec![
+        Person {
+            age: 21,
+            name: "Hadi".to_owned(),
+            fav_color: String::from("Blue"),
+        },
+        Person {
+            age: 22,
+            name: "Yashua".to_owned(),
+            fav_color: String::from("Red"),
+        },
+        Person {
+            age: 20,
+            name: "Haseeb".to_owned(),
+            fav_color: String::from("Yello"),
+        },
+    ];
+
+    // * Iterate through the vector using a for..in loop
+    for person in persons {
+        // * Use an if expression to determine which person's info should be printed
+        if person.age >= 21 {
+            print_data(&person.name, &person.fav_color)
+        }
+    }
+}
