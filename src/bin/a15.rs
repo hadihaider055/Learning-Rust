820c3fceb1c56cda4f8df34a7412871c289892c6
Hadi Haider <haiderhadi055@gmail.com>
2023-04-22 16:54:11 +0500

    Lesson-15: advanced match

--- src/bin/a15.rs
+++ src/bin/a15.rs
@@ -11,4 +11,33 @@
 // * Create one of each ticket and place into a vector
 // * Use a match expression while iterating the vector to print the ticket info
 
-fn main() {}
+// * Use an enum for the tickets with data associated with each variant
+enum Ticket {
+    Backstage(f64, String),
+    Vip(f64, String),
+    Standard(f64),
+}
+
+fn main() {
+    // * Create one of each ticket and place into a vector
+    let tickets: Vec<Ticket> = vec![
+        Ticket::Backstage(32.2, "John Doe".to_owned()),
+        Ticket::Vip(52.2, "Hadi Haider".to_owned()),
+        Ticket::Standard(12.2),
+    ];
+
+    // * Use a match expression while iterating the vector to print the ticket info
+    for ticket in tickets {
+        match ticket {
+            Ticket::Backstage(price, ticketHolder) => println!(
+                "{:?} owns the backstaged ticket which is of {:?}",
+                ticketHolder, price
+            ),
+            Ticket::Vip(price, ticketHolder) => println!(
+                "{:?} owns the backstaged ticket which is of {:?}",
+                ticketHolder, price
+            ),
+            Ticket::Standard(price) => println!("Standard ticket costs {:?}", price),
+        }
+    }
+}
