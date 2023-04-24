70e6698aa0998fbb0461143260bcc2c95b8a4126
Hadi Haider <haiderhadi055@gmail.com>
2023-04-24 16:37:22 +0500

    Lesson-17: Standard Library API Doc

--- src/bin/a17.rs
+++ src/bin/a17.rs
@@ -12,4 +12,8 @@
 //   to uppercase and lowercase
 //   * Try searching for: to_uppercase, to_lowercase
 
-fn main() {}
+fn main() {
+    let name = "Hadi";
+    println!("Name: {:?}", name.to_uppercase());
+    println!("Name: {:?}", name.to_lowercase());
+}
