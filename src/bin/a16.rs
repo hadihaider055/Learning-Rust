44a28fe3cfcf5961aafe934b3c218f94ebc3336e
Hadi Haider <haiderhadi055@gmail.com>
2023-04-24 15:57:57 +0500

    Lesson-16: Option

--- src/bin/a16.rs
+++ src/bin/a16.rs
@@ -9,6 +9,24 @@
 // * The locker assignment should use an Option<i32>
 
 // * Use a struct containing the student's name and locker assignment
-// * The locker assignment should use an Option<i32>
+struct Student {
+    name: String,
+    // * The locker assignment should use an Option<i32>
+    locker: Option<i32>,
+}
 
-fn main() {}
+fn print_student_details(name: &str, locker: Option<i32>) {
+    println!("Student: {:?}", name);
+    match locker {
+        Some(lock) => println!("Locker: {:?}", lock),
+        None => print!("No locker assigned"),
+    }
+}
+
+fn main() {
+    let detail: Student = Student {
+        name: "Hadi Haider".to_owned(),
+        locker: Some(32),
+    };
+    print_student_details(&detail.name, detail.locker)
+}
