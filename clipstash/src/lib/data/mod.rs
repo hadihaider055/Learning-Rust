c8810e6875831ba40a854832d9fa52f707e8a9a3
haiderhadi055 <haiderhadi055@gmail.com>
2023-09-18 01:54:11 +0500

    feat: database setup and clip query

--- clipstash/src/lib/data/mod.rs
+++ clipstash/src/lib/data/mod.rs
@@ -1,10 +1,28 @@
+pub mod model;
+pub mod query;
+
 use derive_more::{Display, From};
 use serde::{Deserialize, Serialize};
+use sqlx::Sqlite;
 use std::str::FromStr;
 use uuid::Uuid;
 
 #[derive(Debug, Clone, From, Display, Serialize, Deserialize)]
 pub struct DbId(Uuid);
+
+#[derive(Debug, thiserror::Error)]
+pub enum DataError {
+    #[error("database error: {0}")]
+    Database(#[from] sqlx::Error),
+}
+
+pub type AppDatabase = Database<Sqlite>;
+pub type DatabasePool = sqlx::sqlite::SqlitePool;
+pub type Transaction<'t> = sqlx::Transaction<'t, Sqlite>;
+pub type AppDatabaseRow = sqlx::sqlite::SqliteRow;
+pub type AppQueryResult = sqlx::sqlite::SqliteQueryResult;
+
+pub struct Database<D: sqlx::Database>(sqlx::Pool<D>);
 
 impl DbId {
     pub fn new() -> DbId {
@@ -26,5 +44,28 @@ impl FromStr for DbId {
     type Err = uuid::Error;
     fn from_str(id: &str) -> Result<Self, Self::Err> {
         Ok(DbId(Uuid::parse_str(id)?))
+    }
+}
+
+impl Database<Sqlite> {
+    pub async fn new(connection_str: &str) -> Self {
+        let pool = sqlx::sqlite::SqlitePoolOptions::new()
+            .connect(connection_str)
+            .await;
+
+        match pool {
+            Ok(pool) => Self(pool),
+            Err(e) => {
+                eprintln!("{}\n", e);
+                eprintln!(
+                    "If the database has not yet been created, run:\n     $ sqlx database setup\n"
+                );
+                panic!("Database connection error");
+            }
+        }
+    }
+
+    pub fn get_pool(&self) -> &DatabasePool {
+        &self.0
     }
 }
